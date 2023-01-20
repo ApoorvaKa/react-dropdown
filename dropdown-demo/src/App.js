fd405fac8ed87e162fc12c99d00902d758f64af6
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-01-20 17:58:40 -0600

    dropdown styling fixes

--- dropdown-demo/src/App.js
+++ dropdown-demo/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <Dropdown list={["option 1", "option 2", "option 3", "option 4"]} multiSelect={true}/>
-      <Dropdown list={["option 1", "option 2", "option 3", "option 4"]} multiSelect={false}/>
+      {/* <Dropdown list={["option 1", "option 2", "option 3", "option 4"]} multiSelect={false}/> */}
     </div>
   );
 }
--- dropdown-demo/src/Dropdown.js
+++ dropdown-demo/src/Dropdown.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Checkbox } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import './Dropdown.css';
 
 function Dropdown({ list, multiSelect = true }) {
@@ -27,35 +29,34 @@ function Dropdown({ list, multiSelect = true }) {
     else setSelectedOptions([])
   };
 
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (!event.path.includes(event.target)) {
-        setOpen(false);
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  const DropdownSelector = () =>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+  }}>
+      <label>
+        Select Option 
+      </label>            
+      <ArrowDropDownIcon fontSize ='large'/> 
+    </div>
 
   return (
     <div className="dropdown">
       <button className="dropdown-button" onClick={() => setOpen(!open)}>
-        {multiSelect ? (selectedOptions.length === 0 ? 'Select options' : selectedOptions.join(', ')) : (selectedOption || 'Select option ')}
+        {multiSelect ? (selectedOptions.length === 0 ? <DropdownSelector/> : selectedOptions.join(', ')) : (selectedOption || <DropdownSelector/>)}
       </button>
       {open && (
         <ul className="dropdown-content">
           {multiSelect && (
             <li>
-              <input type="checkbox" checked={selectAll} onChange={handleSelectAll}/>
+              <Checkbox checked={selectAll} onChange={handleSelectAll}/>
               Select All/Unselect All
             </li>
           )}
           {list.map((option, index) => (
             <li key={index} onClick={() => toggleOption(option)} className={(multiSelect ? selectedOptions : [selectedOption]).includes(option) || selectAll ? 'selected' : ''}>
-              {multiSelect && <input type="checkbox" checked={selectAll || selectedOptions.includes(option)} onChange={() => toggleOption(option)} disabled={selectAll}/>}
+              {multiSelect && <Checkbox checked={selectAll || selectedOptions.includes(option)} onChange={() => toggleOption(option)} disabled={selectAll}/>}
               {option}
             </li>
           ))}
